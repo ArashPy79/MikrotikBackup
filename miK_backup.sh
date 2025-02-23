1791e83d636aa3e6fccd2089f525e8ccbe6a5f99
Arash <146343684+ArashPy79@users.noreply.github.com>
2025-02-23 05:00:46 +0330

    Update miK_backup.sh

--- miK_backup.sh
+++ miK_backup.sh
@@ -1,1 +1,1 @@
-curl "https://api.telegram.org/bot7588522569:AAFL1wwZAhYHQzP3UCAwefm0Cjk1uWdWCnI/sendMessage?chat_id=7317041958&text=پیام%20مورد%20نظر%20شما%20اینجا%20می‌آید"
+curl "https://api.telegram.org/bot7588522569:AAFL1wwZAhYHQzP3UCAwefm0Cjk1uWdWCnI/sendMessage?chat_id=7317041958&text=$FILE_PATH"
