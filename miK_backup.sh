1efc6891df3124ff6f56829df5d8e5b16f318d0f
Arash <146343684+ArashPy79@users.noreply.github.com>
2025-02-23 05:47:17 +0330

    Update miK_backup.sh

--- miK_backup.sh
+++ miK_backup.sh
@@ -1,5 +1,8 @@
- curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
+FILE_NAME=$(basename "$FILE_PATH")
+
+curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
 -H "Content-Type: multipart/form-data" \
 -F "chat_id=$CHAT_ID" \
--F "document=@$FILE_PATH" \
+-F "document=@$FILE_PATH;filename=new_filename.txt" \
 -F "caption=----------------\nwww.one3erver.com"
+-F "
