64e6c581031338328d9e45382f3a95ab86ae3a42
Arash <arashasadi13799@gmail.com>
2025-02-23 04:42:12 +0330

    Create miK_backup.sh
    .

---
+++ miK_backup.sh
@@ -0,0 +1,5 @@
+curl -X POST \"https://api.telegram.org/bot$BOT_TOKEN/sendDocument\" 
+  -H \"Content-Type: multipart/form-data\" 
+  -F \"chat_id=$CHAT_ID\" 
+  -F \"document=@$FILE_PATH\" 
+  -F \"caption=----------------\nwww.one3erver.com\"
