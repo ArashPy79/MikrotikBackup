c7e43e682b042f1a5f9e527ecbb91f8626e9ac34
Arash <arashasadi13799@gmail.com>
2025-02-23 04:50:14 +0330

    Update miK_backup.sh
    .

--- miK_backup.sh
+++ miK_backup.sh
@@ -1,5 +1,1 @@
-curl -X POST \"https://api.telegram.org/bot$BOT_TOKEN/sendDocument\" 
-  -H \"Content-Type: multipart/form-data\" 
-  -F \"chat_id=$CHAT_ID\" 
-  -F \"document=@$FILE_PATH\" 
-  -F \"caption=----------------\nwww.one3erver.com\"
+curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" -H "Content-Type: multipart/form-data" -F "chat_id=$CHAT_ID" -F "document=@$FILE_PATH" -F "caption=----------------\nwww.one3erver.com"
